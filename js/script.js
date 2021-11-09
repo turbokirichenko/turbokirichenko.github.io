757c891e6b45df8bdebe652431ea497038a1f063
turbokirichenko <turbokirichenko@outlook.com>
2021-11-09 11:37:30 -0800

    last working set

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+function O(i){
+	return typeof i == 'object' ? i : document.getElementById(i)
+}
+
+function S(i){
+	return O(i).style
+}
+
+function C(i){
+	return document.getElementByClassName(i)
+}
+
+
+//#MAIN OBJECTS#//
+
+
+//Site
+
+
+//Page
+
+
+//Composition
+
+
+//#DEPEND OBJECTS#//
+
+//Animal
+
+
+//Picture
